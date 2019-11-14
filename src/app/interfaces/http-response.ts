a103e7de62f74fab24b0ecd08c8195a07c0a4653
Israel Oluwole <olaleyeisrael@gmail.com>
2019-11-14 21:50:17 +0100

    bm: minor change

--- src/app/interfaces/http-response.ts
+++ src/app/interfaces/http-response.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15be88a7332071f234f4fca7fc489bdeb11b8e83fb87a7c59f443dbb8e99c8c8
-size 296
+oid sha256:e86b6f335814aba5c8b149dc0590ea2cf9646b789cc064a58cccc2d8848ff439
+size 322
