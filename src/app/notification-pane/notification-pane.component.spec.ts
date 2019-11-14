41e48821a6c2474bd0bb7d1a1b7331c7f7838f67
Israel Oluwole <olaleyeisrael@gmail.com>
2019-11-14 23:40:47 +0100

    bm: separated notification pane

---
+++ src/app/notification-pane/notification-pane.component.spec.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:44ccee5ab9e3730037bb06485b7b10e1fb98626adec93b95c8ae0b916480867d
+size 699
---
+++ src/app/notification-pane/notification-pane.component.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:631f4d5210bffa4af1f9ad6824663c41db7ab3d30b39f7eee1e868a717fb9476
+size 312
