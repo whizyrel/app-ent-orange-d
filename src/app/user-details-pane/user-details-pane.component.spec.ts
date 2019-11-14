70f7773555139ee53d150e1483938d4fd60a219d
Israel Oluwole <olaleyeisrael@gmail.com>
2019-11-14 23:46:47 +0100

    bm: separation

---
+++ src/app/user-details-pane/user-details-pane.component.spec.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:48d63910124f9f69e2d8c652fb8ceb569d9a390110ee519c2137fd700333252f
+size 693
---
+++ src/app/user-details-pane/user-details-pane.component.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3f54077b77d6e943bf68ca39ef28a306834089d209a8f4af2bc6d4329e87f7b6
+size 311
