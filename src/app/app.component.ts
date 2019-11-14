b654754c075b7154f1e81d990758e11a7f8082d3
Israel Oluwole <olaleyeisrael@gmail.com>
2019-11-14 09:42:37 +0100

    bm: method call: save details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f841b83efe3591ad1452a802ad705b733c664518d7cc44778d12b36a112d6c5f
-size 804
+oid sha256:fa133131b04e8bbfd4146ca761bcff5345b4f53f6999b87cddd537fa43e0660d
+size 862
