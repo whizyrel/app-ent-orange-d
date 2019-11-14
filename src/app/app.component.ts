cfcf0dc30f003e48b807cf77bce78e1019553c07
Israel Oluwole <olaleyeisrael@gmail.com>
2019-11-14 08:15:58 +0100

    bm: beta get details imp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d45f6051888a30c266d930eb3b49dbc3fcdafce8d93d533bc64cda8cc2f1282d
-size 681
+oid sha256:d384fa99f96ef4c0a166d291a216187c0387264cffc26952f7dc5073f0f2897e
+size 688
