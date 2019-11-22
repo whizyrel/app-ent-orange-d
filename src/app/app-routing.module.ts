2cd07c6ce45fc604dc45ae9c22ed7a3bdf5614b4
Israel Oluwole <olaleyeisrael@gmail.com>
2019-11-22 14:22:28 +0100

    bm: minor changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:75e6f70ded3c726fb967b3730bc753d015a7eb929eee58e25ad94a89ee2bbe1d
-size 1413
+oid sha256:b0ed9e4f77d5e7fc4121a20793ba5505dec549546dfdb612c65948a8838cbc8a
+size 1435
