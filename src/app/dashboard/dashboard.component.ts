03cfa59d4e36091750d8cbd9b91e20b57881ca22
Israel Oluwole <olaleyeisrael@gmail.com>
2019-11-22 18:29:23 +0100

    bm: removed outer outlet, pane switch

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:beb644cc70190a336db9d534cf6540c06138083f388d9a9506d566819b48ba44
-size 589
+oid sha256:ab6dfbfa6e5d9d00b6977c175a4755fa39036a6c3d730bf020c5b204697f15c1
+size 1504
