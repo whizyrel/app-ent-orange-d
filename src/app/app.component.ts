72aa4c0310ba966fdd1aeb8ebcd6b80be10d6bbe
Israel Oluwole <olaleyeisrael@gmail.com>
2019-12-02 22:04:20 +0100

    bm: fixed undefined

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d07adaa77a156d82578ff11640def78185800cb8aa9c65e031f81df0a330f2e4
-size 928
+oid sha256:73d271ddebd1389bbc89e6cc6cc8528ee59ff35f54b60d882ef52fd8954119d6
+size 931
