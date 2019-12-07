107cb50322322badef465da0d5db0f14e3b68336
Israel Oluwole <olaleyeisrael@gmail.com>
2019-12-07 11:30:37 +0100

    bm: bind and value change

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4629d9ad91d44b188fef454a10665f4ef5aef52a9a20ad0cc2c7de7ec510bbc9
-size 1049
+oid sha256:911f0d3eee8d2cadb0468269eb36c0e79c7b32d29522d78b57f587055bd3107e
+size 1042
