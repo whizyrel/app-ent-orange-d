8507609aa491cefa6a3b5200e255404e6aed9968
Israel Oluwole <olaleyeisrael@gmail.com>
2019-12-07 11:18:00 +0100

    bm: beta imp of list rendering... corrupt

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eead5d454c33e81b37ee4dbe1402b3ed44d7b090d16d50bc8897365ec449eafe
-size 1020
+oid sha256:4629d9ad91d44b188fef454a10665f4ef5aef52a9a20ad0cc2c7de7ec510bbc9
+size 1049
