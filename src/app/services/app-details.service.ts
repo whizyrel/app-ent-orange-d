6e80e1fd883835caeaf0edd5f1689e6fc00de501
Israel Oluwole <olaleyeisrael@gmail.com>
2019-12-07 13:32:17 +0100

    bm: added new method

--- src/app/services/app-details.service.ts
+++ src/app/services/app-details.service.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:71bdaac40d9bbb9ca657b7afbeb9f95ba6efd8b5ba2533ad7a4f97237ff14032
-size 1240
+oid sha256:72db3a78b2b90025e1e7a3d34c9eac659878383ac0cc842eab28e37d3e4c7a54
+size 1371
