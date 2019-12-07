c5cfbd9882a88acdef38c6cc72811f948eed079c
Israel Oluwole <olaleyeisrael@gmail.com>
2019-12-07 11:16:22 +0100

    bm: assets

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:169b3d4960c81451bd89e1921f7414bb4c11013b4b02483f9b5a55ba37efeab5
-size 261
+oid sha256:b6e117e56c8e3bc838c403bf5df81d2b46773f8d838baf7312b3f5b034c6cb7f
+size 355
