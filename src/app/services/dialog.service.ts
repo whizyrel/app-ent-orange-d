c930f765b8c043fd121e62f32fee1a72d27ad0ac
Israel Oluwole <olaleyeisrael@gmail.com>
2019-12-11 18:36:26 +0100

    bm: removed backdrop

--- src/app/services/dialog.service.ts
+++ src/app/services/dialog.service.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e82b5a4291c8d78415c67e8fff8781d62374ac0415af770194ce508a2ebf4194
-size 724
+oid sha256:df0591f5244e6acdeb9a6640b67b7e86d3232679d1cb2bf9a74b03f0bb84bda1
+size 721
