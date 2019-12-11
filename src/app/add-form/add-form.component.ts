b927ac2e44e6273009eec8b19d8873c26b85c4fe
Israel Oluwole <olaleyeisrael@gmail.com>
2019-12-11 18:37:09 +0100

    bm: added method for closing dialog

--- src/app/add-form/add-form.component.ts
+++ src/app/add-form/add-form.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:19f67df3662a09b0a313ee74d8f4b91c7f5b904f7571b72d40b4f96daa38fffc
-size 528
+oid sha256:a4cfbdba8f846f0a6913eaf2620c812dc2ef652fe845fcac00ca932db7cc77dd
+size 589
