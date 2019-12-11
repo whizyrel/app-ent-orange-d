3f566a74e2c404f50187e3eb7bb39af03ecfd38d
Israel Oluwole <olaleyeisrael@gmail.com>
2019-12-11 18:27:15 +0100

    bm: entry component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf68c6e300ebdf2640899b5a8c345fcffdae32580b645fabffa8b3cf39d1f8e6
-size 2070
+oid sha256:14f59487986913c50c6f106779c2663ea5eedb64113ae451949784fb76bd2b2e
+size 2109
