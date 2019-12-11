48946a8104b034f08832fa44c5da50af228ae9b6
Israel Oluwole <olaleyeisrael@gmail.com>
2019-12-11 14:15:08 +0100

    bm: imported dialog service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:220c6b86b43226422c81497cc749b22090a0f367d954f36da2fb7f0fb1c78132
-size 1992
+oid sha256:cf68c6e300ebdf2640899b5a8c345fcffdae32580b645fabffa8b3cf39d1f8e6
+size 2070
