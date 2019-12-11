0020e6e5d249eb4905cab266726ceb870f975733
Israel Oluwole <olaleyeisrael@gmail.com>
2019-12-11 12:26:03 +0100

    bm: implemented key spying

--- src/app/interfaces/form-details.ts
+++ src/app/interfaces/form-details.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a6c8eb25bcdacaa50305d97a5b888d326ee9dd2816c0cdcf0c308b67121d8d6
-size 108
+oid sha256:c725a9395425d3c8e2459e2a127ca41a9e8ec244e13394404586e3f35b636df2
+size 130
