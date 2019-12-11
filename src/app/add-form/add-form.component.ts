b3b506f1d5f7029a7f40daff02edee28d9d5774f
Israel Oluwole <olaleyeisrael@gmail.com>
2019-12-11 18:29:40 +0100

    bm: beta design

--- src/app/add-form/add-form.component.ts
+++ src/app/add-form/add-form.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac3ce0fc24a94983e094ce2072f539028415c1f4077ae5e703fb908141511119
-size 276
+oid sha256:19f67df3662a09b0a313ee74d8f4b91c7f5b904f7571b72d40b4f96daa38fffc
+size 528
