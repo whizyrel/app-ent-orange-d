b7c10dbfc0490d8cce15184a57b70b5b37508f90
Israel Oluwole <olaleyeisrael@gmail.com>
2019-12-11 13:36:28 +0100

    bm: material wahala

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa9f7147d78876c146df4065d00ea41a80d1ff9472131fdb0ebf5d089e1f9163
-size 1823
+oid sha256:220c6b86b43226422c81497cc749b22090a0f367d954f36da2fb7f0fb1c78132
+size 1992
