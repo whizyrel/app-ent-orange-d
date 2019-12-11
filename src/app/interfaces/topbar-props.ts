ba79aafc70b67817897786a3b0f002ff17aaa42a
Israel Oluwole <olaleyeisrael@gmail.com>
2019-12-11 19:27:29 +0100

    bm: added props

--- src/app/interfaces/topbar-props.ts
+++ src/app/interfaces/topbar-props.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:81c01fad551d3495039a4f2250ce8d0c74500dd115ed0202f3cfd5c004f52631
-size 105
+oid sha256:897cee83bfce553c191181a174bb448881f7ddb8e406a4630b45447929ce88f8
+size 134
