32509113c5a7889ea4d79c24d569d67561d40a34
Israel Oluwole <olaleyeisrael@gmail.com>
2019-12-11 19:34:00 +0100

    bm: temp routerlink active imp for topbar links

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93dcf93ef7ee107924835bc0f40d75b3d7911edf69551cf8a62adbc6fc3bf10f
-size 2053
+oid sha256:cf74cc44e14064f350ea333f15a5e7b6ddbb4938adc8355d8a9ef0b38b669742
+size 2009
