d66fb12a949f58ccc27d2be1e098981a937326ec
Israel Oluwole <olaleyeisrael@gmail.com>
2019-12-12 18:48:37 +0100

    bm: imported comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4d7f2c79a82af72dd0d6ecd403588c51c77a2c2625132fda02dc553509b5753
-size 2880
+oid sha256:93c82beb6c6557baa14fb44dea6371946932308a41220d160e00e11c231746a9
+size 2990
