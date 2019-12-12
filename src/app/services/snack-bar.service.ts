3c82694d00f87c3dd484a61596d6ffc13199cfd1
Israel Oluwole <olaleyeisrael@gmail.com>
2019-12-12 17:21:20 +0100

    bm: inserted class

--- src/app/services/snack-bar.service.ts
+++ src/app/services/snack-bar.service.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c68b90ad8e4a8eb45aacf9dceb1f3fa28e741681577e1baa0e68fd0d0074a3c6
-size 505
+oid sha256:5e516d699c7676c6469e43588402883fa010476a00cca1d9ae7e2ec551c2e359
+size 542
