3a84b053b5440b5d0a6e644fd714ad5cfc6bd3e3
Israel Oluwole <olaleyeisrael@gmail.com>
2019-12-12 15:37:53 +0100

    bm: select module import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3153ea90d9729802f59944e9b0b0e1cac303878269b17ce198b1e83a02246431
-size 2612
+oid sha256:4b1ff5cee15afdaad532ca768e9fc723c1040ed8a389b1d747a2ab3d5c4eaa26
+size 2691
