0db4bc771f8306a8877cfbd57ddc582b9ec532de
Israel Oluwole <olaleyeisrael@gmail.com>
2019-12-12 15:38:49 +0100

    bm: added interface for form title props

--- src/app/interfaces/form-details.ts
+++ src/app/interfaces/form-details.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c725a9395425d3c8e2459e2a127ca41a9e8ec244e13394404586e3f35b636df2
-size 130
+oid sha256:aa93d5fd8614f944a8361f79b17c526e1235065313a73ef24e95e24cf5ba93c3
+size 208
