8a52253a94dcde05986512caa5ef1bd491d238fc
Israel Oluwole <olaleyeisrael@gmail.com>
2019-12-12 13:12:38 +0100

    bm: href changes

--- src/app/common/links.ts
+++ src/app/common/links.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6bd16c287c2048277334aad018de0122ba4ee4f1249337e442af00a213a7729e
-size 896
+oid sha256:3409862bb84540821993ae7b1b260887dd2bc66a5ab5b313403fa29e2c5eb856
+size 876
