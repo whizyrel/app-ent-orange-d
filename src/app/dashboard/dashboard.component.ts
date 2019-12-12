1c86ad9fb3df5b97750b82dbb2a7970fc0ed5efd
Israel Oluwole <olaleyeisrael@gmail.com>
2019-12-12 12:01:37 +0100

    bm: open dialog beta imp

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf74cc44e14064f350ea333f15a5e7b6ddbb4938adc8355d8a9ef0b38b669742
-size 2009
+oid sha256:6b3d9b8b9afcff361b2b5cebd06532d27bd6288eaf53f66abd9633da0e3a45e7
+size 1941
--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3291114d6a659919e14cd62c3dfac975ba150ebacc78aa3cf60fc40e50e68b54
-size 1693
+oid sha256:89c01b764ebacc252edbc1c6e0e850acf5897218404bc352acd192af9c7dfb6b
+size 1857
