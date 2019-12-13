55e32f6a841b453d6605cbf1d826bf5064f75b3f
Israel Oluwole <olaleyeisrael@gmail.com>
2019-12-13 07:57:50 +0100

    bm: passing data into add-edit

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f9b881cc92723599c83c1c3c95dc0d6ca6168db7f59c3a6b4ba6404358942f9
-size 2549
+oid sha256:a6263bae4957f8174561fac1e973cc42bd5d65a56e46c457ae983be16d624096
+size 2860
