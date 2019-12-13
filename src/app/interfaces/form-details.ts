2926d3f527f31e0789a2eb19715665ba4f8274ad
Israel Oluwole <olaleyeisrael@gmail.com>
2019-12-13 07:48:33 +0100

    bm: added prop

--- src/app/interfaces/form-details.ts
+++ src/app/interfaces/form-details.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa93d5fd8614f944a8361f79b17c526e1235065313a73ef24e95e24cf5ba93c3
-size 208
+oid sha256:1689ad7089f35dcbe9511ecdd96675b306f4021be7c2be8b9fe5381f1ad3dd05
+size 226
