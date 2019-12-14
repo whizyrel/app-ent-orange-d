5e43c242b1135a90f4dbd9b7ad1e4c6233e7baae
Israel Oluwole <olaleyeisrael@gmail.com>
2019-12-14 10:09:28 +0100

    bm: changed pbindings and data passed

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e2d564c5bf79a2079e42008df73f7f0fa846e7267c811a045a112c6b56e9c38
-size 3106
+oid sha256:306a34d8778f00b1270de43aecb0ac0425b8e5e0a9f82a4ea5d904f2d0a1274a
+size 3033
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e8d304ac1d7af62490cb186fce834ee26b94765f9b8cb2297dbcc660335d236
-size 1817
+oid sha256:f475e708788dc949f3152e8e993f244b5c7c36cd739b0d680632b9fee97e9eaf
+size 1921
