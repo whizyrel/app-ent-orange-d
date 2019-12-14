2a34b7fdbba395bb57e4083498225390e3cad374
Israel Oluwole <olaleyeisrael@gmail.com>
2019-12-14 09:13:50 +0100

    bm: add form props dialog beta imp

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:063e8c6a6e6f5ce97b7ebdc19d404f655e83907744453eb2f3a3bebfa394bba8
-size 1534
+oid sha256:0e8d304ac1d7af62490cb186fce834ee26b94765f9b8cb2297dbcc660335d236
+size 1817
