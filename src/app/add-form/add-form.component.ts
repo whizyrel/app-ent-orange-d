7d8c150de53de820189eb2b8e994f82ded306eb3
Israel Oluwole <olaleyeisrael@gmail.com>
2019-12-14 09:34:32 +0100

    bm: transition to add props beta imp

--- src/app/add-form/add-form.component.ts
+++ src/app/add-form/add-form.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fca8093ca417928058ffebe5020980bd972e64bc8620bb616062f219e6167b30
-size 4793
+oid sha256:3957ca1a6cf563a13aaf1c9f8b2a71d916f9db1ae67d1dd3902669e23b89bc42
+size 5126
