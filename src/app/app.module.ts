028398524988561d3a53928b72cbc4b581a7c0d4
Israel Oluwole <olaleyeisrael@gmail.com>
2019-12-16 21:07:40 +0100

    bm: entry component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,89 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d5553d58ddf8b97a36700c029d80e0055114a1092e062cd0cca0ed835988898a
-size 3229
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgxElectronModule } from 'ngx-electron';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { MatDialogModule } from '@angular/material/dialog';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import {MatCardModule} from '@angular/material/card';
+
+import { AppDetailsService } from './services/app-details.service';
+import {ApiUrlsService} from './services/api-urls.service';
+import {LocalStorageService} from './services/local-storage.service';
+import {DecryptEncryptService} from './services/decrypt-encrypt.service';
+import { FormService } from './services/form.service';
+import { DialogService } from './services/dialog.service';
+import { PermissionsService } from './services/permissions.service';
+import { SnackBarService } from './services/snack-bar.service';
+
+import {Links} from './common/links';
+
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FormsComponent } from './forms/forms.component';
+import { MockPreloaderComponent } from './mock-preloader/mock-preloader.component';
+import { ListComponent } from './list/list.component';
+import { NotificationPaneComponent } from './notification-pane/notification-pane.component';
+import { UserDetailsPaneComponent } from './user-details-pane/user-details-pane.component';
+import { HomeComponent } from './home/home.component';
+import { AddFormComponent } from './add-form/add-form.component';
+import { AddFormPropsComponent } from './add-form-props/add-form-props.component';
+import { DeleteDialogComponent } from './delete-dialog/delete-dialog.component';
+import { DeleteFormPropDialogComponent } from './delete-form-prop-dialog/delete-form-prop-dialog.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    FormsComponent,
+    MockPreloaderComponent,
+    ListComponent,
+    NotificationPaneComponent,
+    UserDetailsPaneComponent,
+    HomeComponent,
+    AddFormComponent,
+    AddFormPropsComponent,
+    DeleteDialogComponent,
+    DeleteFormPropDialogComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgxElectronModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatDialogModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatTabsModule,
+    MatSelectModule,
+    MatSnackBarModule,
+    MatCardModule
+  ],
+  providers: [
+    AppDetailsService,
+    ApiUrlsService,
+    LocalStorageService,
+    DecryptEncryptService,
+    Links,
+    FormService,
+    DialogService, PermissionsService,
+    SnackBarService
+  ],
+  bootstrap: [AppComponent],
+  entryComponents: [
+    AddFormComponent, DeleteDialogComponent,
+    AddFormPropsComponent, DeleteFormPropDialogComponent
+  ]
+})
+export class AppModule {}
