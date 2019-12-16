5536e2b613869d42114551c99034489edf775412
Israel Oluwole <olaleyeisrael@gmail.com>
2019-12-16 06:24:00 +0100

    bm: edit form props beta imp

--- src/app/add-form-props/add-form-props.component.ts
+++ src/app/add-form-props/add-form-props.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b1ed7d95ebf37358dd07c1c8e042e1189773dfcab2b43fc29ed2917eca3d9c2c
-size 2271
+oid sha256:b631db33a49c6616f86cf02a4cd00bb316a7ded7470faea5185e155caf384c2c
+size 2967
