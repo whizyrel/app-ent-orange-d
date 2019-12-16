b5d78f63785bf0dd535895b7da204c35482e1a49
Israel Oluwole <olaleyeisrael@gmail.com>
2019-12-16 06:18:44 +0100

    bm: new interface

--- src/app/interfaces/form-details.ts
+++ src/app/interfaces/form-details.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d98363a235978512abe92114207cfed0d8d181cde4c64706ab1b6e09014d56a8
-size 268
+oid sha256:bf217d9c6a1709e6327693d4a10fc52c4bfaf44aab66e787995faa8a2a1fbca8
+size 371
