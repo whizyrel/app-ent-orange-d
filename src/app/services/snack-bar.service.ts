2fac4020592b70630a36e7e30a8d5536d806760c
Israel Oluwole <olaleyeisrael@gmail.com>
2019-12-16 19:44:08 +0100

    bm: default props

--- src/app/services/snack-bar.service.ts
+++ src/app/services/snack-bar.service.ts
@@ -1,3 +1,29 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5e516d699c7676c6469e43588402883fa010476a00cca1d9ae7e2ec551c2e359
-size 542
+import { Injectable } from '@angular/core';
+
+import { MatSnackBar } from '@angular/material';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SnackBarService {
+  private config: any = {
+    verticalPosition: 'bottom',
+    horizontalPosition: 'right',
+    panelClass: '__mx-vw-33'
+  };
+
+  constructor(
+    private _snackbar: MatSnackBar
+  ) { }
+
+  public showSnackBarFromComponent(): void {
+    // this._snackbar.openFromComponent();
+  }
+
+  public showSnackBar(data: string, action = 'close', config = this.config): void {
+    this._snackbar.open(
+      data, action,
+      config
+    );
+  }
+}
