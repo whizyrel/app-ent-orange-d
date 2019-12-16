16202761df6ac5d371ef7b0f5161386f3b8ec4ae
Israel Oluwole <olaleyeisrael@gmail.com>
2019-12-16 18:07:23 +0100

    bm: lfs smudge

--- src/app/services/form.service.ts
+++ src/app/services/form.service.ts
@@ -1,3 +1,122 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e3691576a09406f8f0366a67ef7d069c96135323b2003c63048112c47d6fecfa
-size 3125
+import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+
+import { ApiUrlsService } from './api-urls.service';
+import { AppDetailsService } from '../services/app-details.service';
+
+import { FormTitle, FormDetails } from '../interfaces/form-details';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FormService {
+  private cid: string = this._appDetails.getClientId;
+
+  constructor(
+    private _http: HttpClient,
+    private _apiUrls: ApiUrlsService,
+    private _appDetails: AppDetailsService
+  ) { }
+
+  public editFormProperty(details: any): Observable<Object> {
+    const _url: string = `${this._apiUrls.formUrls.editProperties}/${this.getClientName}`;
+
+    return this._http.patch<Object>(_url, details, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+      observe: 'body',
+      responseType: 'json',
+    });
+  }
+
+  public addFormProperty(details: any): Observable<Object> {
+    const _url: string = `${this._apiUrls.formUrls.addProperties}/${this.getClientName}`;
+
+    return this._http.post<Object>(_url, details, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+      observe: 'body',
+      responseType: 'json',
+    });
+  }
+
+  public changeFormPermission(details: FormTitle): Observable<Object> {
+    const _url: string = `${this._apiUrls.formUrls.changePermission}/${this.getClientName}`;
+
+    return this._http.patch<Object>(_url, details, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+      observe: 'body',
+      responseType: 'json',
+    });
+  }
+
+  public editFormTitle(details: FormTitle): Observable<Object> {
+    const _url: string = `${this._apiUrls.formUrls.edit}/${this.getClientName}`;
+
+    return this._http.patch<Object>(_url, details, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+      observe: 'body',
+      responseType: 'json',
+    });
+  }
+
+  public deleteFormTitle(id: string): Observable<Object> {
+    const _url: string = `${this._apiUrls.formUrls.delete}/${this.getClientName}/${id}`;
+
+    return this._http.delete<Object>(_url, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+      observe: 'body',
+      responseType: 'json',
+    });
+  }
+
+  public addFormTitle(details: FormTitle): Observable<Object> {
+    const _url: string = `${this._apiUrls.formUrls.add}/${this.getClientName}`;
+
+    return this._http.post<Object>(_url, details, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+      observe: 'body',
+      responseType: 'json',
+    });
+  }
+
+  public getFormDetails(fid: string, ft: string): Observable<Object> {
+    const _url: string = `${this._apiUrls.formUrls.listProperties}/${this.cid}/${ft}/${fid}`;
+
+    return this._http.get<Object>(_url, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+      observe: 'body',
+      responseType: 'json',
+    });
+  }
+
+  public get getList(): Observable<Object> {
+    const _url: string = `${this._apiUrls.formUrls.list}/${this.getClientName}`;
+
+    return this._http.get<Object>(_url, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+      observe: 'body',
+      responseType: 'json',
+    });
+  }
+
+  private get getClientName(): string {
+    return this._appDetails.getName;
+  }
+}
