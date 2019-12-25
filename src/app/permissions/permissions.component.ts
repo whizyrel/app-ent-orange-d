970594fd7c7cc3b5c17309be4ab430511afa8a2b
Israel Oluwole <olaleyeisrael@gmail.com>
2019-12-25 04:11:16 +0100

    bm: add permission beta imp

--- src/app/permissions/permissions.component.ts
+++ src/app/permissions/permissions.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/material';
+
+import { PermissionsService } from '../services/permissions.service';
+import { DialogService } from '../services/dialog.service';
+
+import { Permissions } from '../interfaces/permissions';
+import { HttpResponse } from '../interfaces/http-response';
+
+import { PermissionsDeleteDialogComponent } from '../permissions-delete-dialog/permissions-delete-dialog.component';
+import { AddPermissionsLevelComponent } from '../add-permissions-level/add-permissions-level.component';
 
 @Component({
   selector: 'app-permissions',
@@ -9,12 +19,63 @@ import { Component, OnInit } from '@angular/core';
     './permissions.component.css'
   ]
 })
-export class PermissionsComponent implements OnInit {
+export class PermissionsComponent implements OnInit,
+AfterContentInit {
   private rId: string;
+  public permissions: Permissions[];
 
-  constructor() { }
+  constructor(
+    private _permissions: PermissionsService,
+    private _dialog: DialogService
+  ) { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  ngAfterContentInit() {
+    this.getPermissionsList();
+  }
+
+  public openPermissionLevelDialog(): void {
+    const dg: MatDialogRef<AddPermissionsLevelComponent> =
+    this._dialog.openDialog(
+      {add: true, edit: false},
+      AddPermissionsLevelComponent
+    );
+
+    dg.afterClosed().subscribe(
+      () => this.getPermissionsList()
+    );
+  }
+
+  public openDeleteDialog(id: string): void {
+    const dialogRef: MatDialogRef<PermissionsDeleteDialogComponent, any> =
+    this._dialog.openDialog(
+      {id}, PermissionsDeleteDialogComponent,
+      {
+        width: '450px', height: '240px',
+        minWidth: '450px',
+        hasBackdrop: true
+      }
+    );
+
+    dialogRef.afterClosed().subscribe(
+      () => {}
+    )
+  }
+
+  private getPermissionsList(): void {
+    this._permissions
+    .listPermissionLevels
+    .subscribe(
+      (data: HttpResponse) => {
+        console.log({data});
+        this.permissions = data.permissions;
+        this.rId = this.permissions[0].id;
+      },
+      (error: HttpResponse) => {
+        console.log({error});
+      }
+    );
   }
 
   public setRid(id: string): void {
