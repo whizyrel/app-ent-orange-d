3276754108a6f80592c0382974cef0a17a3d3eaf
Israel Oluwole <olaleyeisrael@gmail.com>
2020-01-02 17:52:43 +0100

    bm: removed inline style

--- src/app/permission-records/permission-records.component.ts
+++ src/app/permission-records/permission-records.component.ts
@@ -21,7 +21,7 @@ export class PermissionRecordsComponent
 
   public records: Array<PermissionRecordsProps>;
   public assignees: string;
-  public mocks: Number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  public mocks: Number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
   constructor(private _permissions: PermissionsService) {}
 
