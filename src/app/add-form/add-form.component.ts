bca52930e5605d157354eb7b755b4169e4dfde47
Israel Oluwole <olaleyeisrael@gmail.com>
2020-01-10 19:19:30 +0100

    bm: self shaking

--- src/app/add-form/add-form.component.ts
+++ src/app/add-form/add-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { FormBuilder, FormControl, FormGroup, Validators, AbstractControl } from '@angular/forms';
 
 import { FormService } from '../services/form.service';
