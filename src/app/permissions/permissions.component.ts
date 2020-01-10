71242bfc15643f8d49599737681b26acbeb5fc43
Israel Oluwole <olaleyeisrael@gmail.com>
2020-01-10 11:50:38 +0100

    bm: open assign dialog beta imp

--- src/app/permissions/permissions.component.ts
+++ src/app/permissions/permissions.component.ts
@@ -20,8 +20,7 @@ import { AssignPermissionComponent } from '../assign-permission/assign-permissio
     './permissions.component.css'
   ]
 })
-export class PermissionsComponent implements OnInit,
-AfterContentInit {
+export class PermissionsComponent implements OnInit, AfterContentInit {
   public rId: string;
   public permissions: Permissions[];
   public mocks: Number[] = [1, 2, 3, 4, 5, 6];
@@ -38,55 +37,65 @@ AfterContentInit {
   }
 
   public openAssignDialog(): void {
-    const dg: MatDialogRef<AssignPermissionComponent> =
-    this._dialog.openDialog(
-      { },
-      AssignPermissionComponent
+    const dg: MatDialogRef<AssignPermissionComponent, any> = this._dialog.openDialog(
+      {},
+      AssignPermissionComponent,
+      {
+        width: 'fit-content',
+        minWidth: '600px',
+        maxWidth: '900px',
+        minHeight: '400px',
+        height: 'auto',
+        maxHeight: '80%',
+        hasBackdrop: true,
+      }
     );
 
-    dg.afterClosed().subscribe(
-      () => this.getPermissionsList()
-    );
+    dg.afterClosed().subscribe(() => this.getPermissionsList());
   }
 
   public openPermissionLevelDialog(): void {
-    const dg: MatDialogRef<AddPermissionsLevelComponent> =
-    this._dialog.openDialog(
-      { },
-      AddPermissionsLevelComponent
+    const dg: MatDialogRef<AddPermissionsLevelComponent> = this._dialog.openDialog(
+      {},
+      AddPermissionsLevelComponent,
+      {
+        width: 'fit-content',
+        minWidth: undefined,
+        minHeight: undefined,
+        hasBackdrop: true,
+        height: undefined
+      }
     );
 
-    dg.afterClosed().subscribe(
-      () => this.getPermissionsList()
-    );
+    dg.afterClosed().subscribe(() => this.getPermissionsList());
   }
 
   public openDeleteDialog(id: string): void {
-    const dialogRef: MatDialogRef<PermissionsDeleteDialogComponent, any> =
-    this._dialog.openDialog(
-      {id}, PermissionsDeleteDialogComponent,
-      {
-      width: '450px',
-      height: '240px',
-      minWidth: '450px',
-      hasBackdrop: true
-    });
+    const dialogRef: MatDialogRef<
+      PermissionsDeleteDialogComponent,
+      any
+      > = this._dialog.openDialog(
+        { id },
+        PermissionsDeleteDialogComponent,
+        {
+          width: '450px',
+          height: '240px',
+          minWidth: '450px',
+          hasBackdrop: true
+        }
+      );
 
-    dialogRef.afterClosed().subscribe(
-      () => this.getPermissionsList()
-    );
+    dialogRef.afterClosed().subscribe(() => this.getPermissionsList());
   }
 
   private getPermissionsList(): void {
-    this._permissions
-    .listPermissionLevels
-    .subscribe(
+    this._permissions.listPermissionLevels.subscribe(
       (data: HttpResponse) => {
         this.permissions = data.permissions;
         this.rId = this.permissions[0].id;
       },
       (error: HttpResponse) => {
-        console.log({error});
+        console.log({ error });
         this.permissions = [];
       }
     );
