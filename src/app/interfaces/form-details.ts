f2afe5b5cc2127529e7696876e69edde0fa7a7f1
Israel Oluwole <olaleyeisrael@gmail.com>
2020-01-10 15:53:02 +0100

    bm: cleanliness

--- src/app/interfaces/form-details.ts
+++ src/app/interfaces/form-details.ts
@@ -4,19 +4,16 @@ export interface FormDetails {
   clientid: string;
   id: string;
   key: Array<string>;
-  // level:  string;
-};
+}
 
 export interface FormTitle {
   title?: string;
-  // permissionLevel?: string;
-  // permissionId?: string;
   id?: string;
-};
+}
 
 export interface FormRecords {
   key?: string;
   type?: string;
   id?: string;
   form_id?: string;
-};
+}
