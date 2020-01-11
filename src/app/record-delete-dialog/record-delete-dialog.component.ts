e2ab4974759aa6ccaa189ce16143e191a037408b
Israel Oluwole <olaleyeisrael@gmail.com>
2020-01-11 18:03:58 +0100

    bm: record delete comp template and style and beta imp

--- src/app/record-delete-dialog/record-delete-dialog.component.ts
+++ src/app/record-delete-dialog/record-delete-dialog.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+
+import { SnackBarService } from '../services/snack-bar.service';
+
+import { HttpResponse } from '../interfaces/http-response';
+import { PermissionsService } from '../services/permissions.service';
 
 @Component({
   selector: 'app-record-delete-dialog',
@@ -6,10 +12,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./record-delete-dialog.component.css']
 })
 export class RecordDeleteDialogComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    private _permissions: PermissionsService,
+    private _snackbar: SnackBarService,
+    private dialogRef: MatDialogRef<RecordDeleteDialogComponent, any>,
+    @Inject(MAT_DIALOG_DATA) private data: any
+  ) {}
 
   ngOnInit() {
+    this.dialogRef.removePanelClass([
+      'cdk-overlay-pane',
+      'mat-dialog-container'
+    ]);
   }
 
+  public close(): void {
+    this.dialogRef.close();
+  }
+
+  public performAction(): void {
+    this.deleteForm(this.data.id);
+  }
+
+  public deleteForm(id: string) {
+    this._permissions
+      .deletePermissionRecord(id)
+      .subscribe(
+      (data: HttpResponse) => {
+        this.close();
+        this._snackbar.showSnackBar(data.message);
+      },
+      (error: HttpResponse) => {
+        console.log({ error });
+        this._snackbar.showSnackBar(error.error.message);
+      }
+    );
+  }
 }
